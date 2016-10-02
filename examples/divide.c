986fd8d997958af455d34a3405bffd2da6a7b291
Christopher Woodall <chris.j.woodall@gmail.com>
2016-10-01 23:21:05 -0400

    Reformatting code

--- examples/divide.c
+++ examples/divide.c
@@ -1,17 +1,16 @@
-#include "option.h"
 #include <stdio.h>
+#include "option.h"
 
 Option<int> divide(int a, int b) {
   if (b == 0) {
     return Nothing();
   } else {
-    return Some(a/b);
+    return Some(a / b);
   }
 }
 
 int main() {
   auto a = divide(100, 2);
-  
 
   if (a) {
     printf("%d\n", a.unwrap());
